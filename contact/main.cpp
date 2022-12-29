a5a17c2711bc5ac4cbb66ea8a999315ec7cf6260
Gary <gaiusmariuschen@gmail.com>
2022-12-29 14:00:49 +0800

    Initial Commit

---
+++ contact/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  contact
+//
+//  Created by Gary Chen on 12/29/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
